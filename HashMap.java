8bd8cbd78cf581d0cef46bacaa12cc18c3286a8b
Camila <cdgolondrina@gmail.com>
2026-03-09 20:59:30 -0600

    función hash personalizada

--- HashMap.java
+++ HashMap.java
@@ -18,4 +18,21 @@ public class HashMap {
         if (capacidad <= 0) capacidad = 10;
         tabla = new Nodo[capacidad];
     }
+
+    //generación de hash
+    private int generarHash(Striing clave) {
+        
+        if (clave == null) return 0;
+        clave = clave.toLowerCase().trim(); //para evitar problemas de mayúsculas o espacios
+
+        long suma = 0L; //long para evitar overflow de int
+
+        for (int i = 0; i < clave.length(); i++) { //recorrer cada caracter
+            int ascii = (int) clave.charAt(i); //obtener valor ascii
+            int peso = i + 1; //peso basado en la posición (1 para el primer caracter, 2 para el segundo...)
+            suma += (long) ascii * peso; //multiplicar ascii por peso y acumular
+        }
+        return (int)((suma % tabla.length + tabla.length) % tabla.length);
+    }
+
 }
